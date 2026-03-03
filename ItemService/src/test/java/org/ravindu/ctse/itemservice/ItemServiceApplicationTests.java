67ceb0c61aee45fe57c10fc4439240bfee1c59de
Ravindu Ishan <bkravidu@gmail.com>
2026-03-04 01:42:09 +0530

    all

---
+++ ItemService/src/test/java/org/ravindu/ctse/itemservice/ItemServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ravindu.ctse.itemservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ PaymentService/src/test/java/org/ravindu/ctse/paymentservice/PaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ravindu.ctse.paymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
